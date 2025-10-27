652981ea44295eb7c37de7b7d8e9197869b6a5ba
Dương Văn Toàn <105204332+toandv2001@users.noreply.github.com>
2025-10-27 23:11:49 +0700

    add gg tag

--- app/app.vue
+++ app/app.vue
@@ -25,6 +25,7 @@ useHead({
     },
   ],
   script: [
+    // ✅ FundraiseUp Script
     {
       innerHTML: `
         (function(w,d,s,n,a){
@@ -42,6 +43,23 @@ useHead({
       type: "text/javascript",
       tagPosition: "head",
     },
+
+    // ✅ Google Tag Manager Script
+    {
+      innerHTML: `
+        (function(w,d,s,l,i){
+          w[l]=w[l]||[];
+          w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;
+          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5SZGFJP2');
+      `,
+      type: "text/javascript",
+      tagPosition: "head",
+    },
   ],
 });
 </script>
@@ -49,5 +67,14 @@ useHead({
 <template>
   <NuxtLayout>
     <NuxtPage />
+    <!-- ✅ GTM noscript fallback -->
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-5SZGFJP2"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript>
   </NuxtLayout>
 </template>
